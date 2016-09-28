855d467c46589ab24cdf5847661ab0bc3406999e
YancyYe <yancy_world@qq.com>
2016-09-28 15:15:09 +0800

    11222222

---
+++ gallerypick/src/main/java/com/yancy/gallerypick/activity/Text.java
@@ -0,0 +1,31 @@
+package com.yancy.gallerypick.activity;
+
+/**
+ * Text
+ * Created by Yancy on 2016/9/28.
+ */
+public class Text {
+
+    private final static String TAG = "Text";
+}
+/*
+ *   ┏┓　　　┏┓
+ * ┏┛┻━━━┛┻┓
+ * ┃　　　　　　　┃
+ * ┃　　　━　　　┃
+ * ┃　┳┛　┗┳　┃
+ * ┃　　　　　　　┃
+ * ┃　　　┻　　　┃
+ * ┃　　　　　　　┃
+ * ┗━┓　　　┏━┛
+ *     ┃　　　┃
+ *     ┃　　　┃
+ *     ┃　　　┗━━━┓
+ *     ┃　　　　　　　┣┓
+ *     ┃　　　　　　　┏┛
+ *     ┗┓┓┏━┳┓┏┛
+ *       ┃┫┫　┃┫┫
+ *       ┗┻┛　┗┻┛
+ *        神兽保佑
+ *        代码无BUG!
+ */
